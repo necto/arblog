f3922c88ceb3b204c7085f4c8100fd17c7acfcff
Necto <necto.ne@gmail.com>
2013-03-25 14:16:42 +0400

    gallery is weakly mounted (in the with-gallery.lisp example)

--- examples/with-gallery.lisp
+++ examples/with-gallery.lisp
@@ -8,12 +8,17 @@
 (asdf:operate 'asdf:load-op '#:arblog-markup-rst)
 ;; (asdf:operate 'asdf:load-op '#:arblog-markup-markdown)
 
-(asdf:operate 'asdf:load-op '#:arblog-theme-mirev)
+(asdf:operate 'asdf:load-op '#:arblog-theme-isimple)
+
+(asdf:operate 'asdf:load-op '#:gallery)
 
 (restas:define-module #:myblog
   (:use #:cl))
 
 (in-package #:myblog)
+
+(restas:mount-module -gallery- (#:gallery)
+  (:url "gal"))
 
 (restas:mount-module -arblog- (#:arblog)
   (arblog:*blog-name* "My blog")
@@ -22,7 +27,7 @@
   (arblog.internal.datastore:*datastore* (make-instance 'arblog.datastore.mongodb:arblog-mongo-datastore))
   (arblog.internal.markup:*markup* (make-instance 'arblog.markup.rst:arblog-rst-markup))
   ;;(arblog.policy.markup:*markup* (make-instance 'arblog.markup.markdown:arblog-markdown-markup))
-  (arblog.internal.theme:*theme* (make-instance 'arblog.theme.mirev:arblog-mirev-theme))
+  (arblog.internal.theme:*theme* (make-instance 'arblog.theme.isimple:arblog-isimple-theme))
   
   (arblog:*disqus-enabled* nil))
 
